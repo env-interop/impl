7bfb17e5e036dfdfc7c43885bf1caa6214c5271e
Paul M. Jones <pmjones@pmjones.io>
2026-04-22 16:50:36 -0500

    test default $_ENV fallback

--- tests/EnvParserTest.php
+++ tests/EnvParserTest.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace EnvInterop\Impl;
 
-use PHPUnit\Framework\Attributes\BackupGlobals;
-
 class EnvParserTest extends \PHPUnit\Framework\TestCase
 {
     protected EnvParser $envParser;
--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace EnvInterop\Impl;
 
+use PHPUnit\Framework\Attributes\BackupGlobals;
+
+#[BackupGlobals(true)]
 class EnvTest extends \PHPUnit\Framework\TestCase
 {
     public function testGetEnv() : void
@@ -17,5 +20,14 @@ class EnvTest extends \PHPUnit\Framework\TestCase
         $env = new Env([123 => 'bar']);
         $this->assertSame('bar', $env->getEnv(123));
         $this->assertSame('bar', $env->getEnv('123'));
+    }
+
+    public function testGetEnv_defaultsToEnvGlobal() : void
+    {
+        $_ENV = ['FOO' => 'from-global', 'BAZ' => 'also-global'];
+        $env = new Env();
+        $this->assertSame('from-global', $env->getEnv('FOO'));
+        $this->assertSame('also-global', $env->getEnv('BAZ'));
+        $this->assertNull($env->getEnv('MISSING'));
     }
 }
