9f584e874861631a66c44ca3c9d98b6b0021781c
Paul M. Jones <pmjones@pmjones.io>
2026-04-22 16:43:20 -0500

    test numeric getter name

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -11,4 +11,11 @@ class EnvTest extends \PHPUnit\Framework\TestCase
         $this->assertSame('bar', $env->getEnv('FOO'));
         $this->assertNull($env->getEnv('BAZ'));
     }
+
+    public function testGetEnv_numericName() : void
+    {
+        $env = new Env([123 => 'bar']);
+        $this->assertSame('bar', $env->getEnv(123));
+        $this->assertSame('bar', $env->getEnv('123'));
+    }
 }
